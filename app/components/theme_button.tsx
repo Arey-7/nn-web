6bf967170046d229d2454d159d9bfecc2d74b8e8
Arey-7 <aaronmulandi@gmail.com>
2023-05-31 20:33:17 +0300

    theme button tweaks

--- app/components/theme_button.tsx
+++ app/components/theme_button.tsx
@@ -3,9 +3,8 @@ import { faSun } from '@fortawesome/free-solid-svg-icons'
 import { faMoon } from "@fortawesome/free-solid-svg-icons";
 
 export default function ThemeButton(){
-    const classStyle = [];
     return(
-        <button className="fixed bottom-3 right-3 bg-black text-white flex p-3 rounded-full hover:invert">
+        <button className="fixed bottom-14 right-3 bg-black text-white flex p-3 rounded-xl text-sm hover:invert">
             <FontAwesomeIcon icon={faSun}/>
         
         </button>
