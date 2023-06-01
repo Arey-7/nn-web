8d0bfc14e575e62fcb3a8e908ccccf430a7bfd3a
Arey-7 <aaronmulandi@gmail.com>
2023-06-01 22:39:33 +0300

    spaced out

--- app/components/theme_button.tsx
+++ app/components/theme_button.tsx
@@ -4,9 +4,9 @@ import { faMoon } from "@fortawesome/free-solid-svg-icons";
 
 export default function ThemeButton(){
     return(
-        <button className="fixed bottom-14 right-3 bg-black text-white flex p-3 rounded-xl text-sm hover:invert">
+        <button className="fixed bottom-14 right-3 bg-black text-white flex p-3 rounded-xl text-sm gap-2 hover:invert">
             <FontAwesomeIcon icon={faSun}/>
-        
+            <FontAwesomeIcon icon={faMoon}/>
         </button>
     );
 };
